42f68b955c8bd48b4c7a564f817ba03edf49030c
shubhsundaram <shubhamloahn551@gmail.com>
2023-10-05 00:52:37 +0530

    new update

--- Advanced.py
+++ Advanced.py
@@ -1,5 +1,6 @@
 # EXCEPTION HANDLING
 
+"""
 print('statement-1')
 try:
     print(10/0)
@@ -14,6 +15,44 @@ try:
     print('x/y = ', x/y)
 except (ZeroDivisionError,ValueError) as msg:
     print('Please enter valid input since :',msg)
+
+
+try:
+    x = int(input("Enter first number:"))
+    y = int(input("Enter second number:"))
+    print("x/y = ",x/y)
+except ZeroDivisionError:
+    print("cannot divide with zero!")
+except:
+    print("Default Except:Please provide valid input")
+"""
+
+
+try:
+    print('Outer try block.')
+    try:
+        print("Inner try block")
+        print(10/0)
+    except ZeroDivisionError:
+        print("Inner except block")
+    finally :
+        print("Inner finally block")
+except:
+    print('Outer except block')
+finally:
+    print('Outer finally block')
+
+
+
+try:
+    print('Try')
+    print(10/0)
+except:
+    print('Except')
+else:
+    print('else')
+finally:
+    print('finally')
 
 
 
