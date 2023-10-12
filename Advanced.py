4262c23bc9392af8a5a90fa6f61b838cf4b8246d
shubhsundaram <shubhamloahn551@gmail.com>
2023-10-12 23:46:43 +0530

    new update

--- Advanced.py
+++ Advanced.py
@@ -1112,10 +1112,15 @@ class Book:
         self.pages = pages
     def __add__(self,other):
         return self.pages + other.pages
+    def __lt__(self,other):
+        return self.pages < other.pages
+    def __ge__(self,other):
+        return self.pages >= other.pages
 b1 = Book(100)
 b2 = Book(300)
 print('The number of total pages = ',b1+b2)
-
+print('b1<b2 : ',b1<b2)
+print('b1>=b2 : ',b1>=b2)
 
 
 
