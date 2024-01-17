382f37943e3292016b4bd3b752079ef5c39f36c9
Subham Kumar <shubhamlohan551@gmail.com>
2024-01-17 23:42:35 +0530

    new updates

--- module1.py
+++ module1.py
@@ -55,5 +55,10 @@ print(output)
 """
 
 
-        
+# List matrix
+list1 = [[1,2,3],[4,5,6],[7,8,9]]
+for i in range(len(list1)):
+    for j in range(len(list1[i])):
+        print(list1[i][j],end=' ')   
+    print()    
 
