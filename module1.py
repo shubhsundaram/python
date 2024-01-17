f5ea143b1cd3aab7799ed6feb8f34673b91d53ff
Subham Kumar <shubhamlohan551@gmail.com>
2024-01-17 22:30:02 +0530

    New update

--- module1.py
+++ module1.py
@@ -1,1 +1,59 @@
-print('This is Module1:')
+"""
+# code to reverse each word.
+s = input('Enter a string : ')
+l = s.split()
+l1 = []
+i = 0
+while i < len(l):
+    l1.append(l[i][::-1])
+    i += 1
+output = ' '.join(l1)
+print(output)
+
+
+# program to merge characters of 2 strings into a single string by taking charaters alternatively.
+s1 = input("Enter a string : ")
+s2 = input("Enter a string : ")
+
+i,j = 0,0
+output = ''
+while i < len(s1) or j < len(s2):
+    if i < len(s1):
+        output += s1[i]
+        i += 1
+    if j < len(s2):
+        output += s2[j]
+        j += 1
+print(output)
+
+
+# write a program to sort the characters of the string and its alphabet symbols are followed by numeric values.
+s = input("Enter a string here : ")
+s1 = s2 = ''
+for x in s:
+    if x.isalpha():
+        s1 += x
+    if x.isdigit():
+        s2 += x
+    
+output = sorted(s1)+sorted(s2)
+output = ''.join(output)
+print(output)
+
+
+s = input("Enter a string here : ")
+output = ''
+for x in s:
+    if x.isalpha():
+        output += x
+        previous = x
+    else:
+        output += previous*(int(x)-1)        
+        
+print(output)
+        
+"""
+
+
+        
+
